977517aea9484b58baed97612d4d25d76f2fc21d
RPletzer-lab <112089261+RPletzer-lab@users.noreply.github.com>
2023-01-18 23:23:27 -0500

    Update index.js
    Signed-off-by: RPletzer-lab <112089261+RPletzer-lab@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from 'WebsiteLab/components/Header';
+import 'WebsiteLab/styles/global.css';
 
 const Home = () => {
     return (
         <div>
+            <Header />
             <h1>Welcome to my website!</h1>
             <p>This is the homepage.</p>
         </div>
